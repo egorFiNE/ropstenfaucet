7abb981f50db18d2fe21d59d85619063ed977a94
Egor Egorov <me@egorfine.com>
2021-09-05 19:03:48 +0300

    proper settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2020',
+    sourcemap: true,
+    emptyOutDir: true,
+    outDir: '../frontend-dist',
+    chunkSizeWarningLimit: '2000k'
+  },
+
   plugins: [vue()]
-})
+});
