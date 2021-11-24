b37e7fa0dd76a264918e8fde2d802c9dea3e38ec
Egor Egorov <me@egorfine.com>
2021-11-24 15:32:18 +0200

    per ip limits

--- backend/index.mjs
+++ backend/index.mjs
@@ -22,7 +22,9 @@ let blockNumberCacheUpdatedAtMs = 0;
 const BLOCK_EXPIRATION_MS = ms('15s');
 const EXPIRATION_SECONDS = 86400;
 const limitsFilename = path.join(__dirname, 'limits.json');
+const ipsFilename = path.join(__dirname, 'ips.json');
 let limits = {};
+let ips = {};
 
 const web3 = new Web3(process.env.ETHEREUM_RPC);
 const sponsor = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
@@ -63,6 +65,18 @@ function storeLimits() {
   fs.writeFileSync(limitsFilename, JSON.stringify(limits, null, "\t"));
 }
 
+function loadIps() {
+  if (!fs.existsSync(ipsFilename)) {
+    return;
+  }
+
+  ips = JSON.parse(fs.readFileSync(ipsFilename));
+}
+
+function storeIps() {
+  fs.writeFileSync(ipsFilename, JSON.stringify(ips, null, "\t"));
+}
+
 function unixtime() {
   return Math.floor(Date.now() / 1000);
 }
@@ -76,6 +90,17 @@ function expireLimits() {
   }
 
   storeLimits();
+}
+
+function expireIps() {
+  const cutoffUnixtime = unixtime() - EXPIRATION_SECONDS;
+  for (const [ ip, lastSentUnixtime ] of Object.entries(ips)) {
+    if (lastSentUnixtime <= cutoffUnixtime) {
+      delete ips[ip];
+    }
+  }
+
+  storeIps();
 }
 
 function readWeiPerAddress() {
@@ -149,7 +174,9 @@ fastify.post('/api/gimme/',
   },
 
   async (request, reply) => {
-    const isCaptchaValidated = await verifyRecaptcha(request.body.token, request.headers['x-real-ip']);
+    const ip = request.headers['x-real-ip'] || '0.0.0.0';
+
+    const isCaptchaValidated = await verifyRecaptcha(request.body.token, ip);
     if (!isCaptchaValidated) {
       return {
         success: true,
@@ -173,6 +200,14 @@ fastify.post('/api/gimme/',
         success: false,
         isLimited: true,
         liftAtUnixtime: limits[addressLC] + EXPIRATION_SECONDS
+      };
+    }
+
+    if (ips[ip]) {
+      return {
+        success: false,
+        isLimited: true,
+        liftAtUnixtime: ips[ip] + EXPIRATION_SECONDS
       };
     }
 
@@ -216,6 +251,9 @@ fastify.post('/api/gimme/',
       limits[addressLC] = unixtime(); // eslint-disable-line require-atomic-updates
       storeLimits();
 
+      ips[ip] = unixtime(); // eslint-disable-line require-atomic-updates
+      storeIps();
+
       console.log("Failed sending to %s", address);
       console.log(e);
 
@@ -228,6 +266,9 @@ fastify.post('/api/gimme/',
     result.once('transactionHash', tx => {
       limits[addressLC] = unixtime(); // eslint-disable-line require-atomic-updates
       storeLimits();
+
+      ips[ip] = unixtime(); // eslint-disable-line require-atomic-updates
+      storeIps();
 
       reply.send({
         success: true,
@@ -249,7 +290,9 @@ fs.watchFile(
 );
 
 loadLimits();
-setInterval(expireLimits, ms('60s'));
+loadIps();
+setInterval(expireLimits, ms('120s'));
+setInterval(expireIps, ms('120s'));
 
 updateFaucetBalance();
 setInterval(updateFaucetBalance, ms('60s'));
